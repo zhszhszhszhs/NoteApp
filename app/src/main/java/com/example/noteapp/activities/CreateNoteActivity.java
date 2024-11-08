021a27c26c83d51e32efab2e1a784de81f76d4d0
zhs <1499455893@qq.com>
2024-11-08 22:05:55 +0800

    Primary Layout Design

---
+++ app/src/main/java/com/example/noteapp/activities/CreateNoteActivity.java
@@ -0,0 +1,24 @@
+package com.example.noteapp.activities;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.noteapp.R;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(v -> onBackPressed());
+    }
+}
---
+++ app/src/main/java/com/example/noteapp/activities/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.noteapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.noteapp.R;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(v -> startActivityForResult(
+                new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE)
+        );
+
+    }
+}
