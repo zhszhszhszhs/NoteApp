9d9b962ef363742fd99fa64554017cb2e85993e8
zhs <1499455893@qq.com>
2024-11-08 22:37:48 +0800

    create & save notes

--- app/src/main/java/com/example/noteapp/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/noteapp/activities/CreateNoteActivity.java
@@ -1,7 +1,13 @@
 package com.example.noteapp.activities;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +16,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.noteapp.R;
+import com.example.noteapp.database.NotesDatabase;
+import com.example.noteapp.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
+
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +35,67 @@ public class CreateNoteActivity extends AppCompatActivity {
 
         ImageView imageBack = findViewById(R.id.imageBack);
         imageBack.setOnClickListener(v -> onBackPressed());
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNoteText);
+        textDateTime = findViewById(R.id.textDateTime);
+        textDateTime.setText(new SimpleDateFormat(
+                "EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(new Date().getTime())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(v -> saveNote());
+    }
+
+    private void saveNote() {
+
+        final String noteTitle = inputNoteTitle.getText().toString().trim();
+        final String noteSubtitle = inputNoteSubtitle.getText().toString().trim();
+        final String noteText = inputNoteText.getText().toString().trim();
+        final String dateTimeStr = textDateTime.getText().toString().trim();
+
+        if (noteTitle.isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (noteSubtitle.isEmpty() && noteText.isEmpty()) {
+            Toast.makeText(this, "Note can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setSubtitle(noteSubtitle);
+        note.setNoteText(noteText);
+        note.setDateTime(dateTimeStr);
+//        note.setColor(selectedNoteColor);
+//        note.setImagePath(selectedImagePath);
+
+//        if (layoutWebURL.getVisibility() == View.VISIBLE) {
+//            note.setWebLink(textWebURL.getText().toString());
+//        }
+//
+//        if (alreadyAvailableNote != null) {
+//            note.setId(alreadyAvailableNote.getId());
+//        }
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getNotesDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+
+                finish();
+            }
+        }
+        new SaveNoteTask().execute();
     }
 }
--- app/src/main/java/com/example/noteapp/activities/MainActivity.java
+++ app/src/main/java/com/example/noteapp/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.noteapp.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.noteapp.R;
+import com.example.noteapp.database.NotesDatabase;
+import com.example.noteapp.entities.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
@@ -24,5 +31,43 @@ public class MainActivity extends AppCompatActivity {
                 new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE)
         );
 
+        getNotes();
+    }
+
+    private void getNotes() {
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNoteTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getNotesDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+//                if (requestCode == REQUEST_CODE_SHOW_NOTES) {
+//                    noteList.addAll(notes);
+//                    notesAdapter.notifyDataSetChanged();
+//                } else if (requestCode == REQUEST_CODE_ADD_NOTE) {
+//                    noteList.add(0, notes.get(0));
+//                    notesAdapter.notifyItemInserted(0);
+//                    notesRecyclerView.smoothScrollToPosition(0);
+//                } else if (requestCode == REQUEST_CODE_UPDATE_NOTE) {
+//                    noteList.remove(noteClickedPosition);
+//                    if (isNoteDeleted) {
+//                        notesAdapter.notifyItemRemoved(noteClickedPosition);
+//                    } else {
+//                        noteList.add(noteClickedPosition, notes.get(noteClickedPosition));
+//                        notesAdapter.notifyItemChanged(noteClickedPosition);
+//                    }
+//                }
+                Log.d("My notes: ", notes.toString());
+            }
+        }
+
+        new GetNoteTask().execute();
     }
 }
